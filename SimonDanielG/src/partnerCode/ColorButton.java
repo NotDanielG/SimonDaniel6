0b9290064b8d1389352e43d4e8b2b515778b4be2
Student 6 <Student 6@10.8.33.163>
2017-01-10 13:01:10 -0500

    Fix

--- SimonDanielG/src/partnerCode/ColorButton.java
+++ SimonDanielG/src/partnerCode/ColorButton.java
@@ -2,6 +2,7 @@ package partnerCode;
 
 import java.awt.Color;
 
+
 import gui.components.Action;
 import gui.components.Button;
 import gui.components.Visible;
@@ -9,7 +10,7 @@ import guiSimon.components.ButtonInterfaceDaniel;
 
 public class ColorButton extends Button implements ButtonInterfaceDaniel, Visible {
 	private int colorIdentifier;
-	private gui.components.Action buttonAction;
+	private Action buttonAction;
 	public ColorButton(int x, int y, int w, int h, String text, Color color, Action action, int value) {
 		super(x, y, w, h, text, color, action);
 		
